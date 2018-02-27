776b4835926b203aa5c5f7d07764f761e2782c93
MichaelTimbes <mtimbes@my.apsu.edu>
2018-02-27 10:16:28 -0600

    Migration to this repo
    Decided to migrate this project to its own repo.

---
+++ AXCEX ONLINE/AXCEX ONLINE/Models/CustomerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AXCEX_ONLINE.Models
+{
+    public class CustomerModel : ApplicationUser
+    {
+        public int ID { get; set; }
+        public string CUSTOMER_NAME { get; set; }
+        public int CUSTOMER_ACCOUNT { get; set; }
+
+    }
+}
